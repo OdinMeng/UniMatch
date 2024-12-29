6599f78c1a05322ba958deb1bc08e266807f9ae3
OdinMeng <mengdino384@gmail.com>
2024-12-29 18:00:24 +0000

    documented query_pinecone.py

--- UniMatch/chatbot/rag/query_pinecone.py
+++ UniMatch/chatbot/rag/query_pinecone.py
@@ -5,9 +5,15 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 
+# Load environment variables
 load_dotenv()
 
-def get_context_from_pineconedb(database_name: str, prompt: str):
+def get_context_from_pineconedb(database_name: str, prompt: str) -> str:
+    """Given a PineCone index, make a query from the prompt and find the closest context. 
+    
+    If no context is available, returns 'None' in string form.
+    """
+    
     pc = Pinecone()
     index = pc.Index(database_name)
 
